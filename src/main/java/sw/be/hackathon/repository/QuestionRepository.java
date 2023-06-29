a35a7e5f25e01568097b7c1909ebeaccd21f80a5
Seungwoo, Joo <thisis.joos@gmail.com>
2023-06-29 13:52:39 +0900

    fix: get 1 random question by subject code only status Y

--- src/main/java/sw/be/hackathon/repository/QuestionRepository.java
+++ src/main/java/sw/be/hackathon/repository/QuestionRepository.java
@@ -8,6 +8,6 @@ import sw.be.hackathon.domain.Question;
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
 
-    @Query(nativeQuery = true, value = "select * from question q where q.subject_code = :code order by RAND() limit 1")
+    @Query(nativeQuery = true, value = "select * from question q where q.subject_code = :code and q.status = 'Y' order by RAND() limit 1")
     Question findByCode(@Param("code") String code);
 }
